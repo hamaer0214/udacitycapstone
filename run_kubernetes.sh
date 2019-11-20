3062af5b94fb337fcc82e9a6a69ce35cf842ff4a
hamaer0214 <36760129+hamaer0214@users.noreply.github.com>
2019-11-20 16:29:35 +0800

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,9 +3,8 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-dockerpath=alchemistbear/nginx
+# minikube start
+minikube start
 
 # Step 2
 # Run the Docker Hub container with kubernetes
