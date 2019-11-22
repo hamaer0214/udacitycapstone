1fbafce24d6d2b58e2cc5c3775e64d37d597990a
hamaer0214 <36760129+hamaer0214@users.noreply.github.com>
2019-11-22 10:02:41 +0800

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,17 +1,13 @@
-#!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
-#script{dockerpath=alchemistbear/nginx-hello1.0}
 
 # Step 2:  
-# Authenticate & tag
-#echo "Docker ID and Image: $dockerpath"
+# tag image
+
 docker tag udacitycapstone alchemistbear/nginx-hello1.0
+
 # Step 3:
 # Push image to a docker repository
 sudo docker push alchemistbear/nginx-hello1.0
